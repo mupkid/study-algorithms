8f1b8bc04832198ecb8634484b3ba97341780509
ohx <mupkid123456@gmail.com>
2022-04-21 22:20:43 +0800

    力扣136：只出现一次的数字

---
+++ leetcode/src/main/java/org/ohx/leetcode/question136/Solution.java
@@ -0,0 +1,21 @@
+package org.ohx.leetcode.question136;
+
+/**
+ * 136：只出现一次的数字
+ * <p>
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * @author mudkip
+ * @date 2022/4/21 21:58
+ */
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
