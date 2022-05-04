05e7b44d8a114683a90d0847c4e911cc717457fa
ouhaoxian <mupkid123456@gmail.com>
2022-05-04 20:49:01 +0800

    《剑指Offer》面试题19：正则表达式匹配

---
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question19/RegularExpressions.java
@@ -0,0 +1,17 @@
+package org.ohx.codinginterviews.question19;
+
+/**
+ * 面试题19：正则表达式匹配
+ * <p>
+ * 题目：请实现一个函数用来匹配包含'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）
+ * 。在本题中，匹配是指字符串的所有字符匹配整个模式。
+ * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"及"ab*a"均不匹配。
+ *
+ * @author mudkip
+ * @date 2022/5/4
+ */
+public class RegularExpressions {
+    public static boolean match(String str, String pattern) {
+        return false;
+    }
+}
---
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question19/Solution.java
@@ -0,0 +1,31 @@
+package org.ohx.codinginterviews.question19;
+
+/**
+ * 面试题19：正则表达式匹配
+ * <p>
+ * 题目：请实现一个函数用来匹配包含'.'和'*'的正则表达式。模式中的字符'.'表示任意一个字符，而'*'表示它前面的字符可以出现任意次（含0次）
+ * 。在本题中，匹配是指字符串的所有字符匹配整个模式。
+ * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"及"ab*a"均不匹配。
+ * <p>
+ * 牛客网版本
+ * 数据范围:
+ * 1.str 只包含从 a-z 的小写字母。
+ * 2.pattern 只包含从 a-z 的小写字母以及字符，和 *，无连续的 '*'。
+ * 3.0 ≤ str.length ≤ 26
+ * 4.0 ≤ pattern.length ≤ 26
+ *
+ * @author mudkip
+ * @since 2022/5/4
+ */
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * @param str     string字符串
+     * @param pattern string字符串
+     * @return bool布尔型
+     */
+    public boolean match(String str, String pattern) {
+        return false;
+    }
+}
