b90ece35a827d4182fbb2e905a8674a4b59b25f4
ouhaoxian <mupkid123456@gmail.com>
2022-05-04 16:28:10 +0800

    《剑指Offer》面试题18：删除链表结点

--- coding-interviews/src/main/java/org/ohx/codinginterviews/question18/DeleteDuplicatedNode.java
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question18/DeleteDuplicatedNode.java
@@ -3,11 +3,13 @@ package org.ohx.codinginterviews.question18;
 /**
  * 面试题18（二）：删除链表中重复的结点
  * <p>
- * 题目：在一个排序的链表中，如何删除重复的结点？例如，在图3.4（a）中重复
- * 结点被删除之后，链表如图3.4（b）所示。
+ * 题目：在一个排序的链表中，如何删除重复的结点
  *
  * @author mudkip
  * @date 2022/5/2
  */
 public class DeleteDuplicatedNode {
+    public static void deleteDuplication() {
+
+    }
 }
