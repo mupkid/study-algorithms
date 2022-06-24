19cfdd0e15b5766f9881c4b90607aae2b6d01575
ohx <mupkid123456@gmail.com>
2022-06-24 21:49:37 +0800

    《剑指Offer》面试题18：在O(1)时间删除链表节点

--- coding-interviews/src/main/java/org/ohx/codinginterviews/question18/DeleteNodeInList.java
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question18/DeleteNodeInList.java
@@ -1,5 +1,9 @@
 package org.ohx.codinginterviews.question18;
 
+import org.ohx.algorithmscommon.linkedlist.ListNode;
+
+import java.util.Objects;
+
 /**
  * 面试题18（一）：在O(1)时间删除链表结点
  * <p>
@@ -9,4 +13,27 @@ package org.ohx.codinginterviews.question18;
  * @date 2022/5/2
  */
 public class DeleteNodeInList {
+    public void DeleteNode(ListNode<Integer> head, ListNode<Integer> toBeDeleted) {
+        if (Objects.isNull(head) || Objects.isNull(toBeDeleted)) {
+            return;
+        }
+
+        ListNode<Integer> next = toBeDeleted.getNext();
+        if (Objects.nonNull(next)) {
+            // 要删除的节点不是尾结点，把下一个节点的内容复制到要删除的节点，然后删除下一个节点
+            toBeDeleted.setValue(next.getValue());
+            toBeDeleted.setNext(next.getNext());
+            next.setNext(null);
+        } else if (head == toBeDeleted) {
+            // 链表只有一个节点，删除头节点
+
+        } else {
+            // 链表有多个节点，删除尾节点
+            ListNode<Integer> temp = head;
+            while (temp.getNext() != toBeDeleted) {
+                temp = temp.getNext();
+            }
+            temp.setNext(null);
+        }
+    }
 }
--- coding-interviews/src/main/java/org/ohx/codinginterviews/question18/Solution.java
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question18/Solution.java
@@ -21,25 +21,25 @@ public class Solution {
      * @return ListNode类
      */
     public ListNode deleteNode(ListNode head, int val) {
-        //加入一个头节点
+        // 加入一个头节点
         ListNode res = new ListNode(0);
         res.next = head;
-        //前序节点
+        // 前序节点
         ListNode pre = res;
-        //当前节点
+        // 当前节点
         ListNode cur = head;
-        //遍历链表
+        // 遍历链表
         while (cur != null) {
-            //找到目标节点
+            // 找到目标节点
             if (cur.val == val) {
-                //断开连接
+                // 断开连接
                 pre.next = cur.next;
                 break;
             }
             pre = cur;
             cur = cur.next;
         }
-        //返回去掉头节点
+        // 返回去掉头节点
         return res.next;
     }
 
