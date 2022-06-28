04d14ffc78732f1d842c8dae53db7f1bf2473920
ohx <mupkid123456@gmail.com>
2022-06-28 20:34:13 +0800

    《剑指Offer》面试题24：反转链表

--- coding-interviews/src/main/java/org/ohx/codinginterviews/question24/ReverseList.java
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question24/ReverseList.java
@@ -1,5 +1,9 @@
 package org.ohx.codinginterviews.question24;
 
+import org.ohx.algorithmscommon.linkedlist.ListNode;
+
+import java.util.Objects;
+
 /**
  * 面试题24：反转链表
  * 题目：定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的头结点。
@@ -8,4 +12,20 @@ package org.ohx.codinginterviews.question24;
  * @date 2022/5/8
  */
 public class ReverseList {
+    public <T> ListNode<T> reverseList(ListNode<T> head) {
+        ListNode<T> reversedHead = null;
+        ListNode<T> node = head;
+        ListNode<T> prev = null;
+        ListNode<T> next;
+        while (Objects.nonNull(node)) {
+            next = node.getNext();
+            if (Objects.isNull(next)) {
+                reversedHead = node;
+            }
+            node.setNext(prev);
+            prev = node;
+            node = next;
+        }
+        return reversedHead;
+    }
 }
