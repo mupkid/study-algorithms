1965f6c9c1b37c010b7df252f7dbef711ff8a706
ohx <mupkid123456@gmail.com>
2022-06-28 20:34:03 +0800

    《剑指Offer》面试题23：链表中环的入口结点

--- coding-interviews/src/main/java/org/ohx/codinginterviews/question23/EntryNodeInListLoop.java
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question23/EntryNodeInListLoop.java
@@ -1,5 +1,9 @@
 package org.ohx.codinginterviews.question23;
 
+import org.ohx.algorithmscommon.linkedlist.ListNode;
+
+import java.util.Objects;
+
 /**
  * 面试题23：链表中环的入口结点
  * <p>
@@ -9,4 +13,42 @@ package org.ohx.codinginterviews.question23;
  * @date 2022/5/8
  */
 public class EntryNodeInListLoop {
+    private <T> ListNode<T> meetingNode(ListNode<T> head) {
+        if (Objects.isNull(head)) {
+            return null;
+        }
+
+        ListNode<T> slow = head.getNext();
+        if (Objects.isNull(slow)) {
+            return null;
+        }
+
+        ListNode<T> fast = slow.getNext();
+        while (Objects.nonNull(fast) && Objects.nonNull(slow)) {
+            if (fast == slow) {
+                return fast;
+            }
+            slow = slow.getNext();
+            fast = fast.getNext();
+            if (Objects.nonNull(fast)) {
+                fast = fast.getNext();
+            }
+        }
+
+        return null;
+    }
+
+    public <T> ListNode<T> entryNodeOfLoop(ListNode<T> head) {
+        ListNode<T> slow = meetingNode(head);
+        if (Objects.isNull(slow)) {
+            return null;
+        }
+
+        ListNode<T> fast = head;
+        while (fast != slow) {
+            fast = fast.getNext();
+            slow = slow.getNext();
+        }
+        return slow;
+    }
 }
