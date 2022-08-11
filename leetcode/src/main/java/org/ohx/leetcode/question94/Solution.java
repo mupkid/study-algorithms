c3b85014d7b525de9cc94b49c1f832f2f538fc8f
ohx <mupkid123456@gmail.com>
2022-08-11 23:36:45 +0800

    力扣94. 二叉树的中序遍历

---
+++ leetcode/src/main/java/org/ohx/leetcode/question94/Solution.java
@@ -0,0 +1,43 @@
+package org.ohx.leetcode.question94;
+
+import java.util.*;
+
+/**
+ * @author mudkip
+ * @date 2022/8/11
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stk = new LinkedList<>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
