1655955672e7d1f09125fbb2829cc73af3c81b56
ohx <mupkid123456@gmail.com>
2022-08-14 14:27:17 +0800

    《剑指Offer》面试题38：字符串的排列

--- coding-interviews/src/main/java/org/ohx/codinginterviews/question38/Solution.java
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/question38/Solution.java
@@ -16,33 +16,6 @@ import java.util.Arrays;
  * @date 2022/8/13
  */
 public class Solution {
-    public void recursion(ArrayList<String> res, char[] str, StringBuffer temp, boolean[] vis) {
-        // 临时字符串满了加入输出
-        if (temp.length() == str.length) {
-            res.add(new String(temp));
-            return;
-        }
-        // 遍历所有元素选取一个加入
-        for (int i = 0; i < str.length; i++) {
-            // 如果该元素已经被加入了则不需要再加入了
-            if (vis[i]) {
-                continue;
-            }
-            if (i > 0 && str[i - 1] == str[i] && !vis[i - 1]) {
-                // 当前的元素str[i]与同一层的前一个元素str[i-1]相同且str[i-1]已经用过了
-                continue;
-            }
-            // 标记为使用过
-            vis[i] = true;
-            // 加入临时字符串
-            temp.append(str[i]);
-            recursion(res, str, temp, vis);
-            // 回溯
-            vis[i] = false;
-            temp.deleteCharAt(temp.length() - 1);
-        }
-    }
-
     public ArrayList<String> Permutation(String str) {
         ArrayList<String> res = new ArrayList<String>();
         if (str == null || str.length() == 0) {
@@ -59,5 +32,35 @@ public class Solution {
         //递归获取
         recursion(res, charStr, temp, vis);
         return res;
+    }
+
+    private void recursion(ArrayList<String> res, char[] str, StringBuffer temp, boolean[] vis) {
+        // 临时字符串满了加入输出
+        if (temp.length() == str.length) {
+            res.add(new String(temp));
+            return;
+        }
+        // 遍历所有元素选取一个加入
+        for (int i = 0; i < str.length; i++) {
+            if (vis[i] || (i > 0 && str[i - 1] == str[i] && !vis[i - 1])) {
+                // 剪枝条件：
+                // 1、元素已经访问过
+                // 2、元素和前一个相同，而且前一个元素已经访问过了
+                // 因为已经按照字典序对元素进行排序了，而且也保证了是从左到右开始选择元素进行添加的
+                // 不需担心走到下一层后用过的元素又被用一次
+                continue;
+            }
+
+            // 使用元素，加入临时字符串，标记为使用过
+            vis[i] = true;
+            temp.append(str[i]);
+
+            // 回溯
+            recursion(res, str, temp, vis);
+
+            // 该子树下的递归结束了，移除
+            temp.deleteCharAt(temp.length() - 1);
+            vis[i] = false;
+        }
     }
 }
