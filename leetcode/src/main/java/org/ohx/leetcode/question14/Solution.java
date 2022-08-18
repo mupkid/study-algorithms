9f498eb499a6d5a1b2a4036cc54c764ef600a9ad
ohx <mupkid123456@gmail.com>
2022-08-18 15:08:34 +0800

    力扣14题：最长公共前缀

---
+++ leetcode/src/main/java/org/ohx/leetcode/question14/Solution.java
@@ -0,0 +1,25 @@
+package org.ohx.leetcode.question14;
+
+import java.util.Arrays;
+
+/**
+ * @author mudkip
+ * @date 2022/8/18
+ */
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String min = strs[0];
+        String max = strs[strs.length - 1];
+        StringBuilder sb = new StringBuilder();
+        int length = min.length() < max.length() ? min.length() : max.length();
+        for (int i = 0; i < length; i++) {
+            if (min.charAt(i) == max.charAt(i)) {
+                sb.append(min.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return sb.toString();
+    }
+}
