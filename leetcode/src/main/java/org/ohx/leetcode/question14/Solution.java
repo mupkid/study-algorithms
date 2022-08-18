12c98e503b0c1ac245c6ec21c751edc62156775b
ohx <mupkid123456@gmail.com>
2022-08-18 15:08:46 +0800

    力扣14题：最长公共前缀

--- leetcode/src/main/java/org/ohx/leetcode/question14/Solution.java
+++ leetcode/src/main/java/org/ohx/leetcode/question14/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
         String min = strs[0];
         String max = strs[strs.length - 1];
         StringBuilder sb = new StringBuilder();
-        int length = min.length() < max.length() ? min.length() : max.length();
+        int length = Math.min(min.length(), max.length());
         for (int i = 0; i < length; i++) {
             if (min.charAt(i) == max.charAt(i)) {
                 sb.append(min.charAt(i));
