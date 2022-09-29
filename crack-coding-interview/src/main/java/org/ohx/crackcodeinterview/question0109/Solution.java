65ff36a80a9d2a413e8aef9f96c833a6d60d50b4
ohx <mupkid123456@gmail.com>
2022-09-29 22:08:58 +0800

    《程序员面试金典》面试题1.9：字符串轮转

---
+++ crack-coding-interview/src/main/java/org/ohx/crackcodeinterview/question0109/Solution.java
@@ -0,0 +1,14 @@
+package org.ohx.crackcodeinterview.question0109;
+
+/**
+ * @author mudkip
+ * @date 2022/9/29
+ */
+public class Solution {
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        return (s2 + s2).contains(s1);
+    }
+}
