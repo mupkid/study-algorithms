67b12f8bdebc3b5ae99bfe6b7a9f4644a57b58d6
ohx <mupkid123456@gmail.com>
2022-09-29 22:34:03 +0800

    《程序员面试金典》面试题17.09：第 k 个数

---
+++ crack-coding-interview/src/main/java/org/ohx/crackcodeinterview/question1709/Solution.java
@@ -0,0 +1,29 @@
+package org.ohx.crackcodeinterview.question1709;
+
+/**
+ * @author mudkip
+ * @date 2022/9/29
+ */
+public class Solution {
+    public int getKthMagicNumber(int k) {
+        int[] result = new int[k];
+        result[0] = 1;
+        int point3 = 0;
+        int point5 = 0;
+        int point7 = 0;
+        for (int i = 1; i < k; i++) {
+            int resultN = Math.min(Math.min(result[point3] * 3, result[point5] * 5), result[point7] * 7);
+            if (resultN % 3 == 0) {
+                point3++;
+            }
+            if (resultN % 5 == 0) {
+                point5++;
+            }
+            if (resultN % 7 == 0) {
+                point7++;
+            }
+            result[i] = resultN;
+        }
+        return result[k - 1];
+    }
+}
