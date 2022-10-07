166a2d53bfbe756a96a8cfcd15b4d4723b157c86
ohx <mupkid123456@gmail.com>
2022-10-07 12:22:35 +0800

    力扣1800. 最大升序子数组和

---
+++ leetcode/src/main/java/org/ohx/leetcode/question1800/README.md
@@ -0,0 +1,9 @@
+# [1800. 最大升序子数组和](https://leetcode.cn/problems/maximum-ascending-subarray-sum/)
+
+给你一个正整数组成的数组`nums`，返回`nums`中一个**升序**子数组的最大可能元素和。
+
+子数组是数组中的一个连续数字序列。
+
+已知子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，若对所有 i（l <= i < r），numsi < numsi+1 都成立，则称这一子数组为 升序 子数组。
+
+注意，大小为 1 的子数组也视作**升序**子数组。
---
+++ leetcode/src/main/java/org/ohx/leetcode/question1800/Solution.java
@@ -0,0 +1,21 @@
+package org.ohx.leetcode.question1800;
+
+/**
+ * @author mudkip
+ * @date 2022/10/7
+ */
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int n = nums.length;
+        for (int i = 1, cur = nums[0]; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                cur += nums[i];
+            } else {
+                cur = nums[i];
+            }
+            max = Math.max(max, cur);
+        }
+        return max;
+    }
+}
