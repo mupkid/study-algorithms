e60ab8d580019839348e3b492a1afab746c87bb4
ohx <mupkid123456@gmail.com>
2022-10-23 11:02:41 +0800

    力扣1768. 交替合并字符串

---
+++ leetcode/src/main/java/org/ohx/leetcode/question1768/Solution.java
@@ -0,0 +1,25 @@
+package org.ohx.leetcode.question1768;
+
+/**
+ * @author mudkip
+ * @date 2022/10/23
+ */
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int i = 0, j = 0;
+
+        StringBuilder ans = new StringBuilder();
+        while (i < m || j < n) {
+            if (i < m) {
+                ans.append(word1.charAt(i));
+                ++i;
+            }
+            if (j < n) {
+                ans.append(word2.charAt(j));
+                ++j;
+            }
+        }
+        return ans.toString();
+    }
+}
