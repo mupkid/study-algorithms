5f5e39adc762a23483991e3542c98c295e4d8158
ohx <mupkid123456@gmail.com>
2022-11-05 15:23:02 +0800

    力扣53. 最大子数组和

---
+++ leetcode/src/main/java/org/ohx/leetcode/question53/Solution.java
@@ -0,0 +1,16 @@
+package org.ohx.leetcode.question53;
+
+/**
+ * @author mudkip
+ * @date 2022/10/30
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
