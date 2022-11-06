c92038cbe237f4de251d2310199ca1f5589efd6b
ohx <mupkid123456@gmail.com>
2022-11-06 10:14:33 +0800

    力扣1678. 设计 Goal 解析器

---
+++ leetcode/src/main/java/org/ohx/leetcode/question1678/Solution.java
@@ -0,0 +1,24 @@
+package org.ohx.leetcode.question1678;
+
+/**
+ * @author mudkip
+ * @date 2022/11/6
+ */
+public class Solution {
+    public String interpret(String command) {
+        // return command.replace("()","o").replace("(al)","al");
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                res.append("G");
+            } else if (command.charAt(i) == '(') {
+                if (command.charAt(i + 1) == ')') {
+                    res.append("o");
+                } else {
+                    res.append("al");
+                }
+            }
+        }
+        return res.toString();
+    }
+}
