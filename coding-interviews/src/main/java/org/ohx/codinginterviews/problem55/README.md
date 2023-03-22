670a83f85557cc3c9ed4f76707fb2fda64141682
ohx <mupkid123456@gmail.com>
2023-03-22 21:27:11 +0800

    《剑指Offer》55 - I. 二叉树的深度

---
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/problem55/README.md
@@ -0,0 +1,5 @@
+# 面试题55 - I. 二叉树的深度
+
+[力扣](https://leetcode.cn/problems/er-cha-shu-de-shen-du-lcof)
+
+同[力扣104](../../../../../../../../leetcode/src/main/java/org/ohx/leetcode/problem104)
---
+++ coding-interviews/src/main/java/org/ohx/codinginterviews/problem55/Solution1.java
@@ -0,0 +1,22 @@
+package org.ohx.codinginterviews.problem55;
+
+
+/**
+ * @author mudkip
+ * @date 2023/3/22
+ */
+public class Solution1 {
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
