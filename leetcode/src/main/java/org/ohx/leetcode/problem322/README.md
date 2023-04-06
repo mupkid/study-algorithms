2579d254293496b708939a24db39f2a38783886e
ohx <mupkid123456@gmail.com>
2023-04-06 22:32:59 +0800

    力扣322. 零钱兑换

---
+++ leetcode/src/main/java/org/ohx/leetcode/problem322/README.md
@@ -0,0 +1,1 @@
+# [322. 零钱兑换](https://leetcode.cn/problems/coin-change)
---
+++ leetcode/src/main/java/org/ohx/leetcode/problem322/Solution.java
@@ -0,0 +1,21 @@
+package org.ohx.leetcode.problem322;
+
+/**
+ * @author mudkip
+ * @date 2023/4/6
+ */
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = amount + 1;
+        }
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
