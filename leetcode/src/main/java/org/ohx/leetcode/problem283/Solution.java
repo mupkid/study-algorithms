b5c7da828838bdb5f908bbec3e4af89b27918854
ohx <mupkid123456@gmail.com>
2023-04-16 22:02:03 +0800

    力扣283. 移动零

---
+++ leetcode/src/main/java/org/ohx/leetcode/problem283/Solution.java
@@ -0,0 +1,24 @@
+package org.ohx.leetcode.problem283;
+
+/**
+ * @author mudkip
+ * @date 2023/4/16
+ */
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
